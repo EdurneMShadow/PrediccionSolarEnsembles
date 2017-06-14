5e4ee553d8ad31d690cdb8ccae2c39b7a95f7f17
EdurneMShadow <edurnecp22@gmail.com>
2017-06-14 23:51:52 +0200

    Modificado el método determinista_a_trihorario_acc, ya que antes no hacía bien el paso de acumulación. Se ha generado una matriz, se supone que correcta, pero no se puede cargar con DataMatrix debido a un fallo con el número de filas.

--- DataMatrix_NWP.py
+++ DataMatrix_NWP.py
@@ -1,27 +1,23 @@
 import pandas as pd
+from itertools import groupby
+from functools import reduce
 import numpy as np
 from datetime import timedelta, datetime
 import os.path
 import re
 
-"""
-general scheme for ensembles matrix generation
+nwp_deterministic_tags = [
+    'FDIR', 'CDIR', 'TCC', 'U10', 'V10', 'T2M', 'SSRD', 'SSR', 'SSRC', 'v10'
+]
 
-1. For day in dates
-  2. For ensemble in range(n_ens)
-     read ensemble file
-     reshape
-     generate index and columns for that ensemble
-     append to last ensemble dataframe as COLUMNS
-  append as ROWS to last day dataframe
-"""
+nwp_ensembles_tags = [
+    'FDIR', 'CDIR', 'TCC', 'U10', 'V10', 'T2M', 'SSRD', 'SSR'
+]
 
 
 def shape_data(data, lon_l, lon_r, lat_l, lat_r, variables, nsteps):
-    idxlon = np.logical_and(data[:, 0] >= lon_l,
-                            data[:, 0] <= lon_r)
-    idxlat = np.logical_and(data[:, 1] >= lat_l,
-                            data[:, 1] <= lat_r)
+    idxlon = np.logical_and(data[:, 0] >= lon_l, data[:, 0] <= lon_r)
+    idxlat = np.logical_and(data[:, 1] >= lat_l, data[:, 1] <= lat_r)
     idx = np.logical_and(idxlon, idxlat)
     data = data[idx]
 
@@ -39,6 +35,7 @@ def group(x):
 
 def to_hourly(df):
     """External function to convert X minute data to hourly data."""
+
     def group(x):
         ts = datetime.strptime(str(x), "%Y%m%d%H%M")
         if ts.minute > 0:
@@ -56,8 +53,7 @@ def to_hourly(df):
 
 
 def select_pen_grid():
-    cols = pd.read_csv(
-        "/scratch/gaa/alecat/dev/cols_ext.txt", sep=';').columns
+    cols = pd.read_csv("/scratch/gaa/alecat/dev/cols_ext.txt", sep=';').columns
     land_grid = []
     for col in cols:
         m = re.search('([-]*\d+[.]\d*), ([-]*\d+[.]\d*)', col)
@@ -67,17 +63,30 @@ def select_pen_grid():
     return sorted({tuple(x) for x in land_grid})
 
 
-def create_dataframe(year, pathin, pathout, hourly=True, model='deterministic'):
+def create_dataframe(year,
+                     pathin,
+                     pathout,
+                     hourly=True,
+                     model='deterministic',
+                     tags=nwp_deterministic_tags,
+                     n_ens=50):
     """Creates a dataframe out of a DataMatrix containing 1 year of hourly
 aggregated data (source files are 15 minute)."""
     dfs = []
     # pathin = '/scratch/gaa/alecat/data/eumetsat/processed/myp/15min/'
     # pathout = '/scratch/gaa/alecat/data/eumetsat/processed/'
     for t in range(365):
-        m = DataMatrix(datetime(year + 1, 1, 1) - timedelta(days=t),
-                       pathin, pathout, delta=1, model=model)
-        dfs.append(m.dataMatrix if not (type(m.dataMatrix) is bool) else
-                   pd.DataFrame())
+        m = DataMatrix(
+            datetime(year + 1, 1, 1) - timedelta(days=t),
+            pathin,
+            pathout,
+            delta=1,
+            tags=tags,
+            model=model,
+            n_ens=n_ens)
+        dfs.append(
+            m.dataMatrix
+            if not (type(m.dataMatrix) is bool) else pd.DataFrame())
     if not hourly:
         return remove_dups(pd.concat(map(to_hourly, dfs))), dfs[0].columns
     return remove_dups(pd.concat(dfs)), dfs[0].columns
@@ -88,8 +97,11 @@ def select_grid(df, latlons):
 latlons."""
     pathin = '/scratch/gaa/alecat/data/eumetsat/processed/myp/15min/'
     pathout = '/scratch/gaa/alecat/data/eumetsat/processed/'
-    m = DataMatrix(datetime.strptime(str(df.index.max()), "%Y%m%d%H"),
-                   pathin, pathout, delta=0)
+    m = DataMatrix(
+        datetime.strptime(str(df.index.max()), "%Y%m%d%H"),
+        pathin,
+        pathout,
+        delta=0)
     m = m.data_matrix_from_data_frame(df)
     return m.query_subgrid(latlons=latlons)
 
@@ -98,18 +110,6 @@ def remove_dups(df):
     idx = np.unique(df.index, return_index=True)[1]
     data = df.iloc[idx]
     return np.hstack((np.matrix(data.index).T, data.values))
-
-
-def get_nwp_deterministic_tags():
-    return ['FDIR', 'CDIR', 'TCC',
-            'U10', 'V10', 'T2M',
-            'SSRD', 'SSR', 'SSRC']
-
-
-def get_nwp_ensembles_tags():
-    return ['FDIR', 'CDIR', 'TCC',
-            'U10', 'V10', 'T2M',
-            'SSRD', 'SSR']
 
 
 class AreaGrid(object):
@@ -143,6 +143,10 @@ class AreaGrid(object):
         return self.res
 
 
+# Sotavento grid 0.25 resolution
+stv_grid = AreaGrid(44.00, -9.5, 42.25, -6.00, 0.25)
+
+
 class DataMatrix(object):
     '''
     Template for the dataMatrix model, the most important attribute is
@@ -159,9 +163,22 @@ class DataMatrix(object):
     date_format = '%Y%m%d%H'
     file_format = '%Y%m%d'
 
-    def __init__(self, date, pathin, pathout, ncoord=2, delta=31, n_ens=50,
-                 grid=False, latlons=False, ifexists=False, create=True,
-                 dm=False, suffix="", freq='D', model='deterministic'):
+    def __init__(self,
+                 date,
+                 pathin,
+                 pathout,
+                 ncoord=2,
+                 delta=31,
+                 n_ens=50,
+                 grid=False,
+                 latlons=False,
+                 ifexists=False,
+                 create=True,
+                 dm=False,
+                 suffix="",
+                 freq='D',
+                 tags=nwp_deterministic_tags,
+                 model='deterministic'):
         '''
         Builds a dataMatrix object by reading up to DELTA MYP files stored
         in PATHIN. The PATHOUT argument indicates the path where the final
@@ -176,11 +193,10 @@ class DataMatrix(object):
             date.strftime(self.date_format) + '.mdata' + suffix + '.npy'
         self.date = date
 
+        self.tags = tags
         if model == 'deterministic':
-            self.tags = get_nwp_deterministic_tags()
             nsteps = 24
         elif model == 'ensembles':
-            self.tags = get_nwp_ensembles_tags()
             self.n_ens = n_ens
             nsteps = 9
 
@@ -197,8 +213,7 @@ class DataMatrix(object):
         if not latlons:
             latlons = self.grid.get_lats_lons()
 
-        self.cols = self.query_cols(latlons=latlons,
-                                    tags=self.tags)
+        self.cols = self.query_cols(latlons=latlons, tags=self.tags)
 
         if os.path.isfile(self.file) and ifexists:
             data = np.load(self.file)
@@ -207,9 +222,9 @@ class DataMatrix(object):
             if model == 'deterministic':
                 cols = list(self.cols)
             else:
-                cols = list(np.array([[c + ' {}'.format(ens)
-                                       for c in self.cols]
-                                      for ens in range(self.n_ens)]).flatten())
+                cols = list(
+                    np.array([[c + ' {}'.format(ens) for c in self.cols]
+                              for ens in range(self.n_ens)]).flatten())
             self.dataMatrix = pd.DataFrame(data, index=index, columns=cols)
         elif create:
             self.dataMatrix = False
@@ -234,9 +249,8 @@ class DataMatrix(object):
         lon_l, lon_r = self.grid.get_lons()
         lat_l, lat_r = self.grid.get_lats()
 
-        for date in pd.date_range(self.date - timedelta(days=delta),
-                                  self.date,
-                                  freq=freq):
+        for date in pd.date_range(
+                self.date - timedelta(days=delta), self.date, freq=freq):
             fecha = datetime.strftime(date, self.file_format)
             files = self.path + fecha + ".myp.npy"
             print(files)
@@ -246,19 +260,18 @@ class DataMatrix(object):
 
             datas = np.load(files)
 
-            datas = shape_data(datas, lon_l, lon_r, lat_l,
-                               lat_r, len(self.tags), nsteps)
+            datas = shape_data(datas, lon_l, lon_r, lat_l, lat_r,
+                               len(self.tags), nsteps)
 
             index = self.query_index(date, date, 'H')
 
-            df = pd.DataFrame(datas, index=index,
-                              columns=self.cols)
+            df = pd.DataFrame(datas, index=index, columns=self.cols)
 
             if isinstance(self.dataMatrix, bool):
                 self.dataMatrix = df
             else:
-                self.dataMatrix = pd.concat([self.dataMatrix, df],
-                                            join='outer', axis=0)
+                self.dataMatrix = pd.concat(
+                    [self.dataMatrix, df], join='outer', axis=0)
 
     def generate_matrix_ensembles(self, delta, freq, nsteps):
         '''
@@ -274,13 +287,13 @@ class DataMatrix(object):
         lon_l, lon_r = self.grid.get_lons()
         lat_l, lat_r = self.grid.get_lats()
 
-        for date in pd.date_range(self.date - timedelta(days=delta),
-                                  self.date,
-                                  freq=freq):
+        for date in pd.date_range(
+                self.date - timedelta(days=delta), self.date, freq=freq):
             df_ens = False
             for ens in range(self.n_ens):
                 fecha = datetime.strftime(date, self.file_format)
-                files = self.path + fecha + "_{}.myp.npy".format(ens)
+                files = self.path + fecha
+                files += "_{}.myp.npy".format(ens) if self.n_ens > 1 else ".myp.npy"
                 print(files)
 
                 if not os.path.isfile(files):
@@ -288,34 +301,37 @@ class DataMatrix(object):
 
                 datas = np.load(files)
 
-                datas = shape_data(datas, lon_l, lon_r, lat_l,
-                                   lat_r, len(self.tags), nsteps)
+                datas = shape_data(datas, lon_l, lon_r, lat_l, lat_r,
+                                   len(self.tags), nsteps)
 
                 index = self.query_index(date, date, '3H', ens)
                 cols = [c + ' {}'.format(ens) for c in self.cols]
 
-                df = pd.DataFrame(datas, index=index,
-                                  columns=cols)
+                df = pd.DataFrame(datas, index=index, columns=cols)
 
                 if isinstance(df_ens, bool):
                     df_ens = df
                 else:
-                    df_ens = pd.concat([df_ens, df],
-                                       join='outer', axis=1)
+                    df_ens = pd.concat([df_ens, df], join='outer', axis=1)
             if isinstance(self.dataMatrix, bool):
                 self.dataMatrix = df_ens
             else:
                 if not isinstance(df_ens, bool):
-                    self.dataMatrix = pd.concat([self.dataMatrix, df_ens],
-                                                join='outer', axis=0)
+                    self.dataMatrix = pd.concat(
+                        [self.dataMatrix, df_ens], join='outer', axis=0)
 
     def data_matrix_from_data_frame(self, df):
         '''
         Build a new DataMatrix copying the properties of self but with the
         inner DataFrame being replaced by df.
         '''
-        return DataMatrix(self.date, self.path, self.out,
-                          grid=self.grid, create=False, dm=df)
+        return DataMatrix(
+            self.date,
+            self.path,
+            self.out,
+            grid=self.grid,
+            create=False,
+            dm=df)
 
     def query_index(self, start_date, end_date, freq, ens=False):
         '''
@@ -332,23 +348,22 @@ class DataMatrix(object):
                 files = self.path + \
                     date.strftime(self.file_format) + '.myp.npy'
                 if os.path.isfile(files):
-                    for hour in pd.date_range(start_date,
-                                              end_date +
-                                              timedelta(days=1) -
-                                              timedelta(hours=1),
-                                              freq=freq):
+                    for hour in pd.date_range(
+                            start_date,
+                            end_date + timedelta(days=1) - timedelta(hours=1),
+                            freq=freq):
                         ts = hour.strftime(self.date_format)
                         index.append(int(ts))
             else:
-                files = self.path + \
-                    date.strftime(self.file_format) + '_{}.myp.npy'.format(ens)
+                files = self.path + date.strftime(self.file_format)
+                files += '_0.myp.npy' if self.n_ens > 1 else ".myp.npy"
                 if os.path.isfile(files):
-                    for hour in pd.date_range(start_date,
-                                              end_date +
-                                              timedelta(days=1),
-                                              freq=freq):
+                    for hour in pd.date_range(
+                            start_date, end_date + timedelta(days=1),
+                            freq=freq):
                         ts = hour.strftime(self.date_format)
                         index.append(int(ts))
+                # index = index[:-1]
 
         return sorted(index)
 
@@ -364,11 +379,11 @@ class DataMatrix(object):
             lon_l, lon_r = grid.get_lons()
             lat_l, lat_r = grid.get_lats()
             res = grid.get_res()
-            latlons = [(i, j) for i in np.arange(lat_l, lat_r + res, res)
+            latlons = [(i, j)
+                       for i in np.arange(lat_l, lat_r + res, res)
                        for j in np.arange(lon_l, lon_r + res, res)]
 
-        return np.array([['({0}, {1}) {2}'.format(j, i, tag)
-                          for tag in tags]
+        return np.array([['({0}, {1}) {2}'.format(j, i, tag) for tag in tags]
                          for i, j in latlons]).flatten()
 
     def query_subgrid(self, grid=None, latlons=None, inplace=False):
@@ -380,9 +395,8 @@ class DataMatrix(object):
         resulting submatrix.
         '''
         if grid is not None or latlons is not None:
-            result = self.dataMatrix.loc[:, self.query_cols(grid=grid,
-                                                            latlons=latlons,
-                                                            tags=self.tags)]
+            result = self.dataMatrix.loc[:, self.query_cols(
+                grid=grid, latlons=latlons, tags=self.tags)]
         else:
             result = self.dataMatrix
 
@@ -390,8 +404,12 @@ class DataMatrix(object):
             self.dataMatrix = result
         return result
 
-    def query_subgrid_dates(self, start_date, end_date,
-                            grid=None, latlons=None, inplace=False):
+    def query_subgrid_dates(self,
+                            start_date,
+                            end_date,
+                            grid=None,
+                            latlons=None,
+                            inplace=False):
         '''
         This method returns a subset of the general matrix following the
         above method, for a farm and dates between start_date and
@@ -405,11 +423,32 @@ class DataMatrix(object):
         else:
             submatrix = self.dataMatrix
 
-        result = submatrix.loc[self.query_index(
-            start_date, end_date, self.freq), :]
+        result = submatrix.loc[self.query_index(start_date, end_date,
+                                                self.freq), :]
         if inplace:
             self.dataMatrix = result
         return result
+
+    def group_by_variable(self, variable):
+        '''
+        Averages the value of variable on the whole grid.
+        '''
+
+        def keyf(col):
+            return col.rsplit()[-1]
+
+        grid = {}
+        for t in self.tags:
+            grid[t] = []
+
+        cols = sorted(self.dataMatrix.columns)
+        groups = map(lambda x: [x[0], list(x[1])[0]], groupby(cols, key=keyf))
+
+        for k, v in groups:
+            grid[k].append(v)
+
+        return reduce(lambda acc, c: acc + self.dataMatrix[c].fillna(0),
+                      grid[variable], 0) / len(grid[variable])
 
     def group_by_hour(self):
         def group(x):
--- desagregar.py
+++ desagregar.py
@@ -3,25 +3,43 @@ import datetime
 import pandas as pd
 import numpy as np
 import sunrise as sr
-import utilities as ut
+import libdata as ut
 
 def guardar_matriz(matriz, fecha, sufijo=''):
     nombre_fichero = '/scratch/gaa/edcastil/'+ str(fecha) + '.mdata' + sufijo
     datos = np.hstack([matriz.index[:,np.newaxis], matriz.values])
+    columnas = matriz.columns
     np.save(nombre_fichero, datos)
+    print('Matriz guardada')
+    nombre_fichero_columnas = nombre_fichero + '.columns'
+    np.save(nombre_fichero_columnas,columnas)
 
 def determinista_a_trihorario_acc():
-    # Estas líneas pasan el modelo determinista horario acumulado a trihorario acumulado.
-    matrix=dm.DataMatrix(datetime.datetime(2015,12,31),'/scratch/gaa/data/solar_ecmwf/deterministic/myp/','/scratch/gaa/data/solar_ecmwf/',ifexists=True,model='deterministic',suffix='.det')
+    # Estas líneas pasan el modelo determinista horario acumulado a trihorario acumulado.+
+    tags = ['FDIR', 'CDIR','SSRD', 'SSR', 'SSRC']
+    matrix=dm.DataMatrix(datetime.datetime(2015,12,31),'/scratch/gaa/data/solar_ecmwf/','/scratch/gaa/data/solar_ecmwf/',ifexists=True,model='deterministic',suffix='.det_noacc_vmodule')
     #matrix.dataMatrix #He creado un objeto de la clase DataMatrix, que tiene un atributo dataMatrix.
     #matrix.dataMatrix.columns #Accede a las columnas de la matriz
-
+    m = matrix.dataMatrix
     land_grid = dm.select_pen_grid() #Selecciona solo las longitudes y latitudes de España.
 
     pen_cols = matrix.query_cols(latlons=land_grid,tags=['FDIR','SSR','SSRC','CDIR','SSRD']) #Estoy seleccionando solo las variables de radiacion (array)
     rad_var = matrix.dataMatrix[pen_cols] #submatriz solo con las columnas de las variables de radiación.
+
+    #Agregar: sumar una fila con la anterior. En la última fila se tendrá la acumulación de todas las anteriores
+    dia = 1
+    for i in range(len(rad_var)-1):
+        print(dia)
+        if dia<24:
+            rad_var.values[i+1] = rad_var.values[i+1] + rad_var.values[i]
+            dia+=1
+        else:
+            dia = 1
+
+
     dates = pd.date_range('20150101','20160101',freq='3H')[:-1]
     index = np.array([int(d.strftime("%Y%m%d%H")) for d in dates])#lista de indices
+
     rad_var_3h = rad_var.loc[index] #extraer el conjunto trihorario que queremos de la matriz
     rad_var_3h = rad_var_3h.diff() #diferencia: un elemento menos el anterior (por filas)
 
@@ -37,6 +55,7 @@ def determinista_a_trihorario_acc():
     rad_var_3h.loc[index_night] = 0 #sobrescribe en la matriz actual
     fecha = matrix.date.strftime(matrix.date_format)
     guardar_matriz(rad_var_3h, fecha, sufijo=".det_3h_acc")
+    return rad_var_3h
 
 '''Estas líneas pasan el clear-sky horario no acumulado a horario acumulado.'''
 def CS_a_acumulado():
@@ -73,4 +92,28 @@ def CS_a_acumulado():
     cs_3h.loc[index_night] = 0 #sobrescribe en la matriz actual
     cs_3h.to_csv('cs_3h_acc.csv')
 
-#determinista_a_trihorario_acc()
+def interpolacion():
+    dates = pd.date_range('20150101','20160101',freq='3H')[:-1]
+    index = np.array([int(d.strftime("%Y%m%d%H")) for d in dates])#lista de indices
+    cs_h = cs.loc[index]
+
+    r_pred = pd.DataFrame(index=cs_h.index, columns=rad_var_3h.columns)
+
+    var = 0
+    for i in cs_h.index:
+        cs_horario = cs_h.loc[i]
+        terminacion = str(i)[8:]
+        if int(terminacion) > var:
+            if var == 21:
+                var = 0
+            else:
+                var+=3
+        if len(str(var)) < 2:
+            var_string = '0' + str(var)
+        else:
+            var_string = str(var)
+        indice = int(str(i)[:8] + var_string)
+        cs_acc = cs_3h.loc[indice]
+        rad_acc = rad_var_3h.loc[indice]
+        fila = cs_horario/cs_acc*rad_acc
+        r_pred.loc[indice] = fila
